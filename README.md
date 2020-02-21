052e0b836724b2867d0082f01d81f3bc52b3662d
Kanstantsin Kulak <kkulak24@gmail.com>
2020-02-21 14:53:20 +0300

    regexp-basics-is-it-a-vowel

---
+++ README.md
@@ -0,0 +1,5 @@
+https://www.codewars.com/kata/regexp-basics-is-it-a-vowel/
+
+Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u), and false otherwise.
+
+FUNDAMENTALS REGULAR EXPRESSIONS DECLARATIVE PROGRAMMING ADVANCED LANGUAGE FEATURES STRINGS
---
+++ tests.js
@@ -0,0 +1,10 @@
+describe('vowel', () => {
+    it('Basic tests', () => {
+      Test.assertEquals(''.vowel(), false);
+      Test.assertEquals('a'.vowel(), true);
+      Test.assertEquals('E'.vowel(), true);
+      Test.assertEquals('ou'.vowel(), false);
+      Test.assertEquals('z'.vowel(), false);
+      Test.assertEquals('lol'.vowel(), false);
+    });
+  });
